cd3da0aca1534646b6b65f07e93f125015454f0a
Emerson Barros <emersonfb99@gmail.com>
2024-12-07 20:06:40 -0300

    feat(colorscheme): add WinSeparator color override

--- lua/emerson/plugins/colorscheme.lua
+++ lua/emerson/plugins/colorscheme.lua
@@ -5,10 +5,13 @@ return {
   config = function()
     require('kanagawa').setup {
       compile = true,
+
+      ---@param colors KanagawaColorsSpec
       overrides = function(colors)
         return {
           IndentLine = { fg = colors.palette.dragonBlack5 },
           IndentLineCurrent = { fg = colors.palette.dragonBlue2 },
+          WinSeparator = { fg = colors.palette.dragonBlack5 },
         }
       end,
     }
