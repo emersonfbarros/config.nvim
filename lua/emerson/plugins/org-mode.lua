a737a2f5deb5b71e88e4b98fd4a021b1249d9c99
Emerson Barros <emersonfb99@gmail.com>
2024-12-07 20:07:44 -0300

    feat(orgmode): adds more tasks states and logbook
    - removes unnecessary filetype declaration

--- lua/emerson/plugins/org-mode.lua
+++ lua/emerson/plugins/org-mode.lua
@@ -7,10 +7,11 @@ return {
       opts = {},
     },
   },
-  ft = { 'org' },
   opts = {
     org_agenda_files = '~/Org/**/*',
     org_default_notes_file = '~/Org/refile.org',
+    org_todo_keywords = { 'TODO', 'IN-PROGRESS', '|', 'DONE', 'CANCELED' },
+    org_log_into_drawer = 'LOGBOOK',
     mappings = {
       org = {
         org_toggle_checkbox = '<M-Space>',
