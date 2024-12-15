66199745250d3ab7bb676e9f8437e05cf1d4e6c9
Emerson Barros <emersonfb99@gmail.com>
2024-12-15 13:44:34 -0300

    feat(telescope): adds grep search with filtering

--- lua/emerson/plugins/telescope.lua
+++ lua/emerson/plugins/telescope.lua
@@ -1,3 +1,44 @@
+local live_grep_filtered = function(opts)
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.uv.cwd()
+  local finder = require('telescope.finders').new_async_job {
+    command_generator = function(prompt)
+      if not prompt or prompt == '' then
+        return nil
+      end
+
+      local pieces = vim.split(prompt, '  ') -- two spaces
+      local args = { 'rg' }
+      if pieces[1] then
+        table.insert(args, '-e') -- for rg takes regex
+        table.insert(args, pieces[1])
+      end
+
+      if pieces[2] then
+        table.insert(args, '-g') -- for glob option, like `*.lua`
+        table.insert(args, pieces[2])
+      end
+
+      return vim
+        .iter({ args, { '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case' } })
+        :flatten()
+        :totable()
+    end,
+    entry_maker = require('telescope.make_entry').gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  }
+
+  require('telescope.pickers')
+    .new(opts, {
+      prompt_title = 'Live grep (with filtering)',
+      finder = finder,
+      debounce = 100,
+      previewer = require('telescope.config').values.grep_previewer(opts),
+      sorter = require('telescope.sorters').empty(),
+    })
+    :find()
+end
+
 return {
   'nvim-telescope/telescope.nvim',
   cmd = 'Telescope',
@@ -15,10 +56,8 @@ return {
   },
   config = function()
     local telescope = require 'telescope'
-    telescope.load_extension 'fzf'
 
     local open_with_trouble = require('trouble.sources.telescope').open
-
     telescope.setup {
       defaults = {
         prompt_prefix = ' ',
@@ -49,13 +88,15 @@ return {
           },
         },
       },
+      extensions = { fzf = {} },
     }
+    telescope.load_extension 'fzf'
   end,
   keys = {
     -- Some stuff
     {
       '<leader><leader>',
-      "<cmd>Telescope buffers<CR>",
+      '<cmd>Telescope buffers<CR>',
       desc = '[ ] Find existing buffers',
     },
     {
@@ -81,17 +122,17 @@ return {
     },
     {
       '<leader>sf',
-      "<cmd>Telescope find_files<CR>",
+      '<cmd>Telescope find_files<CR>',
       desc = '[S]earch [F]iles',
     },
     {
       '<leader>sg',
-      "<cmd>Telescope live_grep<CR>",
-      desc = '[S]earch by [G]rep',
+      live_grep_filtered,
+      desc = '[S]earch by [G]rep with filters',
     },
     {
       '<leader>sw',
-      "<cmd>Telescope grep_string<CR>",
+      '<cmd>Telescope grep_string<CR>',
       desc = '[S]earch current [W]ord',
     },
     {
@@ -103,43 +144,43 @@ return {
     },
     {
       '<leader>sD',
-      "<cmd>Telescope diagnostics<CR>",
+      '<cmd>Telescope diagnostics<CR>',
       desc = '[S]earch workspace [D]iagnostics',
     },
     {
       '<leader>sk',
-      "<cmd>Telescope keymaps<CR>",
+      '<cmd>Telescope keymaps<CR>',
       desc = '[S]earch [K]eymaps',
     },
     {
       '<leader>sh',
-      "<cmd>Telescope help_tags<CR>",
+      '<cmd>Telescope help_tags<CR>',
       desc = '[S]earch [H]elp',
     },
     {
       '<leader>sr',
-      "<cmd>Telescope registers<CR>",
+      '<cmd>Telescope registers<CR>',
       desc = '[S]earch [R]egisters',
     },
     -- Git stuff
     {
       '<leader>gf',
-      "<cmd>Telescope git_files<CR>",
+      '<cmd>Telescope git_files<CR>',
       desc = '[G]it [F]iles',
     },
     {
       '<leader>gc',
-      "<cmd>Telescope git_commits<CR>",
+      '<cmd>Telescope git_commits<CR>',
       desc = '[G]it [C]ommits',
     },
     {
       '<leader>gs',
-      "<cmd>Telescope git_status<CR>",
+      '<cmd>Telescope git_status<CR>',
       desc = '[G]it [S]tatus',
     },
     {
       '<leader>gS',
-      "<cmd>Telescope git_stash<CR>",
+      '<cmd>Telescope git_stash<CR>',
       desc = '[G]it [S]tashes',
     },
   },
