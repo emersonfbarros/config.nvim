2f8ed0dff7879505334f1e83477fab6056e6c689
Emerson Barros <emersonfb99@gmail.com>
2024-12-15 13:50:34 -0300

    feat(colorscheme): improves colors for win separator and syntax hl

--- lua/emerson/plugins/colorscheme.lua
+++ lua/emerson/plugins/colorscheme.lua
@@ -3,15 +3,30 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    ---@diagnostic disable-next-line: missing-fields
     require('kanagawa').setup {
       compile = true,
+
+      colors = {
+        theme = {
+          dragon = {
+            syn = {
+              parameter = '#a292a3',
+              number = '#a6a69c',
+            },
+            ui = {
+              bg_gutter = 'none',
+            },
+          },
+        },
+      },
 
       ---@param colors KanagawaColorsSpec
       overrides = function(colors)
         return {
           IndentLine = { fg = colors.palette.dragonBlack5 },
           IndentLineCurrent = { fg = colors.palette.dragonBlue2 },
-          WinSeparator = { fg = colors.palette.dragonBlack5 },
+          WinSeparator = { fg = colors.palette.dragonBlack6 },
         }
       end,
     }
