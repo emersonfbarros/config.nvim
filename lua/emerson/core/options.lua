7f299331d65781a63d1d79a5d89e95b47beda045
Emerson Barros <emersonfb99@gmail.com>
2024-12-15 13:49:48 -0300

    feat(options): improves tab listchar and adds diff char

--- lua/emerson/core/options.lua
+++ lua/emerson/core/options.lua
@@ -36,7 +36,9 @@ vim.wo.signcolumn = 'yes'
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '▏ ', trail = '·', nbsp = '␣' }
+
+vim.opt.fillchars:append { diff = "╱" }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
