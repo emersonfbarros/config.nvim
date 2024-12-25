8d002c8cd99a42ed409051dcbc4438262d307270
Emerson Barros <emersonfb99@gmail.com>
2024-12-24 22:31:21 -0300

    feat(lualine): adds field that shows the number of loaded buffers

--- lua/emerson/plugins/lualine.lua
+++ lua/emerson/plugins/lualine.lua
@@ -1,13 +1,11 @@
 return {
   'nvim-lualine/lualine.nvim',
   event = 'VeryLazy',
-  dependencies = {
-    'AndreM222/copilot-lualine',
-  },
+  dependencies = 'AndreM222/copilot-lualine',
   opts = {
     options = {
       icons_enabled = true,
-      component_separators = ' ▎',
+      component_separators = '|',
       section_separators = '',
       globalstatus = true,
       theme = 'kanagawa',
@@ -23,6 +21,18 @@ return {
         'mode',
       },
       lualine_c = {
+        {
+          function()
+            local buffers = vim.api.nvim_list_bufs()
+            local count = 0
+            for _, buf in ipairs(buffers) do
+              if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_name(buf) ~= '' then
+                count = count + 1
+              end
+            end
+            return string.format('(%d)', count)
+          end,
+        },
         {
           'filename',
           path = 1,
