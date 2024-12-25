4b024c967a00768dd7f2a623706cfa59ea2fcca7
Emerson Barros <emersonfb99@gmail.com>
2024-12-24 22:30:31 -0300

    refactor(illuminate): configs large files limit and updates event

--- lua/emerson/plugins/illuminate.lua
+++ lua/emerson/plugins/illuminate.lua
@@ -1,8 +1,13 @@
 return {
   'RRethy/vim-illuminate',
-  event = 'VeryLazy',
+  event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
   config = function()
     require('illuminate').configure {
+      delay = 200,
+      large_file_cutoff = 2000,
+      large_file_overrides = {
+        providers = { 'lsp' },
+      },
       filetypes_denylist = {
         'dirbuf',
         'NeogitStatus',
